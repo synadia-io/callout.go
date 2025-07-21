dbc859fc9578d2305aceb1bc3f314599c6ae5b7a
roeschter <michael@roeschter.de>
2025-07-21 19:19:18 +0200

    New default_sentinel example
    Similar to delegate example
    Creates and configures the default_sentinel in server.conf
    Compares user name and password for authentication

---
+++ examples/delegated_default_sentinel/delegated_default_sentinel.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/nats-io/jwt/v2"
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nkeys"
+
+	"github.com/synadia-io/callout.go"
+)
+
+func loadAndParseKeys(fp string) (nkeys.KeyPair, error) {
+	if fp == "" {
+		return nil, errors.New("key file required")
+	}
+	seed, err := os.ReadFile(fp)
+	if err != nil {
+		return nil, fmt.Errorf("error reading key file: %w", err)
+	}
+	if !bytes.HasPrefix(seed, []byte{'S', 'A'}) {
+		return nil, fmt.Errorf("key must be an account private key")
+	}
+	kp, err := nkeys.FromSeed(seed)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing key: %w", err)
+	}
+	return kp, nil
+}
+
+func getConnectionOptions(fp string) ([]nats.Option, error) {
+	if fp == "" {
+		return nil, errors.New("creds file required")
+	}
+	return []nats.Option{nats.UserCredentials(fp)}, nil
+}
+
+func main() {
+	// load the creds, and keys
+	var credsFp, calloutKeyFp, issuerKeyFp string
+	flag.StringVar(&credsFp, "creds", "", "creds file for the service")
+	flag.StringVar(&calloutKeyFp, "callout-issuer", "", "key for signing callout responses")
+	flag.StringVar(&issuerKeyFp, "issuer", "", "key for signing users")
+	flag.Parse()
+
+	cKP, err := loadAndParseKeys(calloutKeyFp)
+	if err != nil {
+		panic(fmt.Errorf("error loading callout issuer: %w", err))
+	}
+	aKP, err := loadAndParseKeys(issuerKeyFp)
+	if err != nil {
+		panic(fmt.Errorf("error loading callout issuer: %w", err))
+	}
+
+	// the authorizer function
+	authorizer := func(req *jwt.AuthorizationRequest) (string, error) {
+		//Create a new user with defaults. We do not set any limits or permissions for the user in this example.
+		uc := jwt.NewUserClaims(req.UserNkey)
+		if req.ConnectOptions.Username == "" || req.ConnectOptions.Password == "" {
+			// ignore this user
+			return "", nil
+		}
+		if req.ConnectOptions.Username != req.ConnectOptions.Password {
+			// ignore this user
+			return "", nil
+		}
+		fmt.Println("namer: ", uc.Name, "")
+		//Encode the user claim as JWT, set the account and sign with the account signing key.
+		return uc.Encode(aKP)
+	}
+
+	// connect the service with the creds
+	opts, err := getConnectionOptions(credsFp)
+	if err != nil {
+		panic(fmt.Errorf("error loading creds: %w", err))
+	}
+	nc, err := nats.Connect("nats://localhost:4222", opts...)
+	if err != nil {
+		panic(fmt.Errorf("error connecting: %w", err))
+	}
+	defer nc.Close()
+
+	// start the service
+	_, err = callout.NewAuthorizationService(nc, callout.Authorizer(authorizer), callout.ResponseSignerKey(cKP))
+
+	// don't exit until sigterm
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+}
---
+++ examples/delegated_default_sentinel/generate.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -x
+
+# put the nsc artifacts where we can find them
+export TMPDIR=/tmp
+export OUTDIR=$TMPDIR/DA
+export XDG_CONFIG_HOME=$OUTDIR/config
+export XDG_DATA_HOME=$OUTDIR/data
+
+# add an operator
+nsc add operator O
+
+# add and register the system account
+nsc add account SYS
+nsc edit operator --system-account SYS
+
+# add the account we are going to place users in via the callout
+nsc add account A
+# capture the ID (subject) for the account
+ACCOUNT=$(nsc describe account A --json | jq .sub -r)
+
+# add the callout account
+nsc add account C
+# capture the ID (subject) for the callout account
+CALLOUT=$(nsc describe account C --json | jq .sub -r)
+# add the service user, this user is for the callout service to connect to NATS
+nsc add user service
+SERVICE=$(nsc describe user service --json | jq .sub -r)
+# add the sentinel users (no permissions) this is a callout user that will be given
+# to all clients to authorize via the callout
+nsc add user sentinel --deny-pubsub \>
+# We configure a default sentinel. For this purpose the JWT must be marked as a bearer token
+nsc edit user sentinel --bearer
+# the callout account needs to specify the ID of the service user, and the accounts
+# that it can generate authorizations for
+nsc edit authcallout --account C --auth-user $SERVICE --allowed-account $ACCOUNT
+
+# make a server configuration file
+nsc generate config --mem-resolver --config-file /tmp/DA/server.conf
+# extract the creds for the service and callout so we can use them
+nsc generate creds --account C --name service -o $OUTDIR/service.creds
+nsc generate creds --account C --name sentinel -o $OUTDIR/sentinel_bearer.creds
+
+# Add the default sentinel to the service.conf
+sed -i '1i"default_sentinel": "BEARER_JWT",' $OUTDIR/server.conf
+export BEARER_JWT=$(sed -n '2p' $OUTDIR/sentinel_bearer.creds)
+sed -i "s/BEARER_JWT/$BEARER_JWT/g" $OUTDIR/server.conf
+
+# Get the seed (private nkey) for the callout account. 
+# This will be used to sign/encrypt the response from the callout service
+cp "$XDG_DATA_HOME/nats/nsc/keys/keys/A/${CALLOUT:1:2}/${CALLOUT}.nk" $OUTDIR/C.nk
+# Get the seed (private nkey) for the placement account. 
+# This will be used to sign the user JWT for this placement account
+cp "$XDG_DATA_HOME/nats/nsc/keys/keys/A/${ACCOUNT:1:2}/${ACCOUNT}.nk" $OUTDIR/A.nk
+
+
+
