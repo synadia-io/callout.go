03a99d3e2f490e9175b3ea88575a52b6fc812057
roeschter <michael@roeschter.de>
2025-08-18 09:30:51 +0200

    Update generate.sh

--- examples/delegated_default_sentinel/generate.sh
+++ examples/delegated_default_sentinel/generate.sh
@@ -42,6 +42,7 @@ nsc generate creds --account C --name service -o $OUTDIR/service.creds
 nsc generate creds --account C --name sentinel -o $OUTDIR/sentinel_bearer.creds
 
 # Add the default sentinel to the service.conf
+# Fixed line breaks
 sed -i '1i"default_sentinel": "BEARER_JWT",' $OUTDIR/server.conf
 export BEARER_JWT=$(sed -n '2p' $OUTDIR/sentinel_bearer.creds)
 sed -i "s/BEARER_JWT/$BEARER_JWT/g" $OUTDIR/server.conf
